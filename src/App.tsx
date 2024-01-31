111ba0dc26573c12b9fab2ea3290b4f90ef2fa1c
Rajat Gupta <7rgdev@gmail.com>
2024-01-31 14:55:01 +0530

    made API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+// import logo from "./logo.svg";
 import "./App.css";
+import { fetchCountries } from "./network";
 
 /**
  * murali@wrkspot.com
@@ -8,6 +9,16 @@ import "./App.css";
  */
 
 function App() {
+  const [countries, setCountries] = useState<Record<string, any>[]>([]);
+  async function getCountries() {
+    const data = await fetchCountries();
+    console.log(data);
+    setCountries(data);
+  }
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
       <h3>Countries Info</h3>
